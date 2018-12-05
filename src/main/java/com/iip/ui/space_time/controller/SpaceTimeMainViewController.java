28e70f74575c47b58b3b97f9c5b552bf83ca6f1c
淦珺(实习) <ganjun@megvii.com>
2018-12-05 20:25:01 +0800

    基于xyl的代码，上传了space_time模块代码，修改了xyl的代码的命名格式，以及一些代码的简化操作

---
+++ src/main/java/com/iip/ui/space_time/controller/SpaceTimeMainViewController.java
@@ -0,0 +1,128 @@
+package com.iip.ui.space_time.controller;
+
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.List;
+import java.util.ArrayList;
+
+/**
+ * @Author Junnor.G
+ * @Date 2018/12/5 下午6:23
+ */
+class MenuModule{
+    private AnchorPane pane;
+    private String fxmlPath;
+    private String text;
+
+    MenuModule(AnchorPane pane, String fxmlPath, String text){
+        this.pane = pane;
+        this.fxmlPath = fxmlPath;
+        this.text = text;
+    }
+
+    public AnchorPane getPane() {
+        return pane;
+    }
+
+    public String getFxmlPath(){
+        return fxmlPath;
+    }
+
+    public String getText(){
+        return text;
+    }
+}
+
+public class SpaceTimeMainViewController implements Initializable{
+    @FXML
+    private Label LblMainTitle;
+    @FXML
+    private AnchorPane APLoadData;
+    @FXML
+    private AnchorPane APParticiple;
+    @FXML
+    private AnchorPane APEntity;
+    @FXML
+    private AnchorPane APSetting;
+    @FXML
+    private BorderPane BPMainViewPane;
+
+    /**
+     * 标记当前所选择的菜单栏中的AnchorPane
+     */
+    private AnchorPane currentMenuPane;
+    /**
+     * 缓存连接配置界面
+     */
+    private AnchorPane connectionConfigPane;
+    /**
+     * 用来保存所有menu的anchorPane模块的设置信息,避免重复代码,
+     * 每次增加新的menu,只需要在其中初始化函数initialize()将对应信息添加进去就ok了
+     */
+    private List<MenuModule> menuModules;
+
+    @FXML
+    public void exit(MouseEvent mouseEvent){
+        mouseEvent.consume();
+        Platform.exit();
+    }
+    @FXML
+    private void menuButtonClicked(MouseEvent mouseEvent){
+        mouseEvent.consume();
+        AnchorPane selectedMenuPane = (AnchorPane) mouseEvent.getTarget();
+        if (currentMenuPane == selectedMenuPane) return;
+
+        if (currentMenuPane != null){
+            currentMenuPane.getStylesheets().clear();
+            currentMenuPane.getStylesheets().add("menuButton");
+        }
+
+        // 去判断鼠标是否点击在对应的Pane上
+        for (MenuModule menuModule: menuModules){
+            if (menuModule.getPane() == selectedMenuPane){
+                menuModule.getPane().getStylesheets().clear();
+                menuModule.getPane().getStylesheets().add("menuButtonSelected");
+                LblMainTitle.setText(menuModule.getText());
+                try {
+                    FXMLLoader loader = new FXMLLoader();
+                    loader.setLocation(SpaceTimeMainViewController.class.getResource(menuModule.getFxmlPath()));
+                    connectionConfigPane = loader.load();
+                    connectionConfigPane.setPrefSize(BPMainViewPane.getWidth()-60, BPMainViewPane.getHeight()-60);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                BPMainViewPane.setCenter(connectionConfigPane);
+                currentMenuPane = selectedMenuPane;
+                break;
+            }
+        }
+
+
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources){
+        System.out.println("In SpaceTimeMainViewController initialize");
+        menuModules = new ArrayList<MenuModule>();
+        // 加载数据界面
+        menuModules.add(new MenuModule(APLoadData, "../view/LoadDataView.fxml", "Load Data"));
+        // 分词界面
+        menuModules.add(new MenuModule(APParticiple, "../view/ParticipleView.fxml", "Participle"));
+        // 实体界面
+        menuModules.add(new MenuModule(APEntity, "../view/EntityView.fxml", "Entity"));
+        // 设置界面
+        menuModules.add(new MenuModule(APSetting, "../view/SettingView.fxml", "Setting"));
+
+    }
+}
